6a02600099d676aa7c242f144d4f701c949f7bac
Mark Plesko <markples@microsoft.com>
2023-02-09 10:30:35 -0800

    comments

--- get_tests/get_tests.py
+++ get_tests/get_tests.py
@@ -1,3 +1,14 @@
+# Usage:
+# python get_tests.py <path to unmerged test> <paths to test groups>
+#
+# Example:
+# python
+#   get_tests\get_tests.py
+#   d:/r/runtime/artifacts/log/TestRun.xml
+#   d:/r/runtime2/artifacts/log/JIT.Regression.Regression_1.testRun.xml
+#   d:/r/runtime2/artifacts/log/JIT.Regression.Regression_2.testRun.xml
+#   ...
+
 import os
 import sys
 import xml.etree.ElementTree as ET
@@ -8,6 +19,9 @@ def load(file):
     tests = {}
     for test in root.iter('test'):
         attrib = test.attrib
+
+        # Tests are listed a bit differently (.cmd vs .dll, some capitalization).
+        # Normalize here.
         name, _ = os.path.splitext(attrib['name'].replace('\\\\','\\'))
         name = name.lower()
         tests[name] = attrib['time']
@@ -26,18 +40,24 @@ def get(files):
             print("no")
     print(f"len(tests1) = {len(tests1)}")
     print(f"len(tests2) = {len(tests2)}")
+
+    # Used to find information about a specific test
     #for name in tests1.keys():
     #    if "b11553" in name:
     #        print(name)
     #for name in tests2.keys():
     #    if "b11553" in name:
     #        print(name)
+
+    # Print differences
     for name in tests1.keys():
         if name not in tests2.keys():
             print(f"only in 1: {name}")
     for name in tests2.keys():
         if name not in tests1.keys():
             print(f"only in 2: {name}")
+
+    # Extend comparison/reporting here
 
 if __name__ == "__main__":
     get(sys.argv)
